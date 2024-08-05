724c345e6103edfdcb715898dfe85372affc0baa
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2024-08-05 06:18:58 -0600

    vietify

---
+++ src/components/Category/Category.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel
+      title="Title"
+      text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima"
+    >
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+<script setup>
+  //
+</script>
