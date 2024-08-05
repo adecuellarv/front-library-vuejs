47648853e157cf87b1c5ac42de353e079a14aaa3
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2024-08-05 08:34:07 -0600

    fix collapse

--- src/components/Category/Category.vue
+++ src/components/Category/Category.vue
@@ -1,23 +1,21 @@
 <template>
   <v-expansion-panels variant="accordion">
     <v-expansion-panel v-for="cat in categories" :key="cat.categoryId">
-      <v-expansion-panel-title>
-        <div class="custom-header-pexpasion">
-          {{ cat.categoryName }}
-        </div>
-      </v-expansion-panel-title>
-      <v-expansion-panel-content>
+      <v-expansion-panel-title>{{ cat.categoryName }}</v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-expansion-panel-content>
         <v-data-table :headers="headers" :items="items">
           <template v-slot:[`item.image`]="{ item }">
             <v-img :src="item.bookImage" max-width="100" />
           </template>
           <template v-slot:[`item.download`]="{ item }">
             <v-btn :href="item.bookPdf" target="_blank" color="primary">
-              Descargar
+              Ver
             </v-btn>
           </template>
         </v-data-table>
       </v-expansion-panel-content>
+      </v-expansion-panel-text>
     </v-expansion-panel>
   </v-expansion-panels>
 </template>
