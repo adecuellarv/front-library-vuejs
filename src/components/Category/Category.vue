e9fb0593a3658bd97c9413acaabe665df9886095
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2024-08-05 07:55:52 -0600

    list book

--- src/components/Category/Category.vue
+++ src/components/Category/Category.vue
@@ -1,18 +1,19 @@
 <template>
-  <v-expansion-panels
-  v-for="item in categories"
-  :key="item.categoryId"
-  >
-    <v-expansion-panel
-      :title="item.categoryName"
-      text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima"
-    >
+  <v-expansion-panels v-for="item in categories" :key="item.categoryId">
+    <v-expansion-panel :title="item.categoryName">
+      <v-expansion-panel-text>
+        <v-data-table :headers="headers" :items="items"></v-data-table>
+
+
+      </v-expansion-panel-text>
     </v-expansion-panel>
   </v-expansion-panels>
 </template>
 <script setup>
-  import { ref, onMounted } from 'vue';
-  import useCategories from './useCategories';
+import { ref, onMounted } from 'vue';
+import useCategories from './useCategories';
 
-  const { categories } = useCategories();
+const { categories } = useCategories();
+const { headers } = useCategories();
+const { items } = useCategories();
 </script>
--- src/components/Category/useCategories.js
+++ src/components/Category/useCategories.js
@@ -4,18 +4,44 @@ import { ref, onMounted } from 'vue';
 export default function useCategories() {
   const categories = ref([
     {
-        "categoryId": 1,
-        "categoryGuid": "73279921-434d-4f03-8d39-289b15d350fe",
-        "categoryName": "Syfy",
-        "books": null
+      "categoryId": 1,
+      "categoryGuid": "73279921-434d-4f03-8d39-289b15d350fe",
+      "categoryName": "Syfy",
+      "books": null
     },
     {
-        "categoryId": 2,
-        "categoryGuid": "9d7e55c5-b39d-4fb9-959a-08c5c3474911",
-        "categoryName": "Comedia",
-        "books": null
+      "categoryId": 2,
+      "categoryGuid": "9d7e55c5-b39d-4fb9-959a-08c5c3474911",
+      "categoryName": "Comedia",
+      "books": null
     }
-]);
+  ]);
+
+  const items = ref([
+    {
+      "bookId": 4,
+      "bookName": "Interestelar",
+      "bookDescription": "Esta es una descripción del libro",
+      "bookImage": "C:\\Users\\Usuario\\Documents\\projects\\files-exam\\1.jpg",
+      "bookPdf": "C:\\Users\\Usuario\\Documents\\projects\\files-exam\\catalogo_red_de_herrajes.pdf",
+      "category": 1
+    },
+    {
+      "bookId": 5,
+      "bookName": "Star Wars",
+      "bookDescription": "Esta es una descripción del libro",
+      "bookImage": "C:\\Users\\Usuario\\Documents\\projects\\files-exam\\1.jpg",
+      "bookPdf": "C:\\Users\\Usuario\\Documents\\projects\\files-exam\\catalogo_red_de_herrajes.pdf",
+      "category": 1
+    }
+  ]);
+
+  const headers = ref([
+    { title: 'Libro', value: 'bookName', key: 'title', },
+    { title: 'Imagen', value: 'bookImage', key: 'image', },
+    { title: 'Url', value: 'bookPdf', key: 'download', },
+    //{ title: 'Actions', value: 'actions', key: 'actions', sortable: false },
+  ]);
 
   const fetchCategories = async () => {
     try {
@@ -31,6 +57,8 @@ export default function useCategories() {
   });
 
   return {
-    categories
+    categories,
+    items,
+    headers
   };
 }
